6e120d2eece216b953b34f07917cb015823a2520
iSamuraii <albertopastrolin@gmail.com>
2013-09-18 11:00:43 +0200

    improved the home

--- api-home.php
+++ api-home.php
@@ -8,28 +8,33 @@
     <?php
       #start the fetch data part
       $website=htmlentities($_GET["website"]);
+      if(!preg_match ('/http:\/\//', $website))
+	$website="http://".$website;
       $json = file_get_contents("$website/api.php");
       $data = json_decode($json);
       #end the fetch data part
-      echo "<h1>Sezioni:</h1>";
-      echo "<ul>";
-      foreach ($data->sections as $section){
-	  echo "<li><a href=\"api-section.php?website=$website&id=$section->id\"><h2>$section->name</h2></a></li>";
-	  echo "<ul>";
-	  foreach ($section->subsections as $sub){
-	    echo "<li><a href=\"api-section.php?website=$website&id=$sub->id\"><h2>$sub->name</h2></a></li>";
-	  }
-	  echo "</ul>";
-      }
-      echo "</ul>";
-      echo "<h1>Articoli:</h1>";
-      echo "<ul>";
-      foreach ($data->articles as $art){
-	  echo "<li><a href=\"api-topic.php?website=$website&id=$art->id\"><strong>$art->title</strong></a>";
-	  echo " <em>$art->desc</em>";
-	  echo "</li>";
+      if($data->sections){
+	echo "<h1>Sezioni:</h1>";
+	echo "<ul>";
+	foreach ($data->sections as $section){
+	    echo "<li><a href=\"api-section.php?website=$website&id=$section->id\"><strong>$section->name</strong></a></li>";
+	    echo "<ul>";
+	    foreach ($section->subsections as $sub)
+	      echo "<li><a href=\"api-section.php?website=$website&id=$sub->id\"><strong>$sub->name</strong></a></li>";
+	    echo "</ul>";
 	}
-      echo "</ul>";
+	echo "</ul>";
+      }
+      if($data->articles){
+	echo "<h1>Articoli:</h1>";
+	echo "<ul>";
+	foreach ($data->articles as $art){
+	    echo "<li><a href=\"api-topic.php?website=$website&id=$art->id\"><strong>$art->title</strong></a>";
+	    echo " <em>$art->desc</em>";
+	    echo "</li>";
+	  }
+	echo "</ul>";
+      }
      ?>
   </body>
 </html>
