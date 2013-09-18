d604564c98990b4c69a3df4c8f16bac94f8a1f60
iSamuraii <albertopastrolin@gmail.com>
2013-09-18 11:40:29 +0200

    started support for the user

--- api-user.php
+++ api-user.php
@@ -6,10 +6,21 @@
   </head>
   <body>
     <?php
-      $website=htmlentities("http://".$_GET["website"]);
+      $website=htmlentities($_GET["website"]);
       $id=$_GET["id"];
       $json = file_get_contents("$website/api.php?mid=$id");
       $data = json_decode($json);
+      echo "<a href=\"api-home.php?website=$website\"><h2>home</h2></a>";
+      #a quick navigation url to the homepage
+      $mid="m".$id;
+      $user=$data->$mid;
+      echo "<div>";
+      echo "<h2>$user->nickname</h2>";
+      echo "<img src=\"$user->avatar\" /> <br />";
+      echo "<strong>Gruppo: ".$user->group->name."</strong><br />";
+      echo "<strong>Messaggi: $user->messages</strong><br />";
+      echo "<strong>Reputazione: $user->reputation</strong><br />";
+      echo "</div>";
      ?>
   </body>
 </html>
